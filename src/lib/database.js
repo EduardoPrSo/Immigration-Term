ad4eab99f3746253d39b3ba96d18ff524f4b8951
DuDuPrSo <85527054+EduardoPrSo@users.noreply.github.com>
2025-07-31 22:58:30 -0300

    Update database.js

--- src/lib/database.js
+++ src/lib/database.js
@@ -1,5 +1,5 @@
 export const createUser = async (user, accept) => {
-    const res = await fetch(`${process.env.BASE_URL}/api/users`, {
+    const res = await fetch(`https://immigration-term.vercel.app/api/users`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name: user.name, id: user.id, accept }),
@@ -13,13 +13,13 @@ export const createUser = async (user, accept) => {
 }
 
 export const getUsers = async () => {
-    const res = await fetch(`${process.env.BASE_URL}/api/users`)
+    const res = await fetch(`https://immigration-term.vercel.app/api/users`)
     const data = await res.json()
     return data
 }
 
 export const getUserById = async (id) => {
-    const res = await fetch(`${process.env.BASE_URL}/api/users/search?id=${id}`)
+    const res = await fetch(`https://immigration-term.vercel.app/api/users/search?id=${id}`)
     const data = await res.json()
     return data[0]
 }
